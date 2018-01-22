d888681dbab6c8f9cd3899d37710e8b2593df8f1
Sam <sam.ibrahiem@gmail.com>
2018-01-22 14:18:11 +0800

    removed multiple AP build
    this build script will only be used for TL-WR1043ND

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# OpenWrt firmware build script for TP-LINK Archer C7, TL-WR1043ND, TL-WDR4300 and Netgear R6100
+# OpenWrt firmware build script for TP-LINK TL-WR1043ND
 #
 # Added features for the Horizon 2020 Wi-5 project:
 #
@@ -20,47 +20,6 @@
 
 set -e
 
-VERBOSE=""
-
-show_help() {
-cat << EOF
-Usage: ${0##*/} [-hv] <TLWR1043|ARCHERC7|TLWDR4300|nand>
-Build OpenWRT firmware with integrated Click and Open vSwitch for the
-specified wireless router. 
-
-P.S In order to generate an OpenWRT firmware with Click for Netgear R6100, please use the nand option.
-
-  -h    display this help and exit
-  -v    verbose mode
-EOF
-}
-
-parse_arguments() {
-  while getopts ":hv" opt; do
-    case "$opt" in
-      v)
-        VERBOSE="V=s"
-        ;;
-      h)
-        show_help
-        exit 0
-        ;;
-      '?')
-        echo "Invalid option: -$OPTARG" >&2
-        exit 1
-        ;;
-    esac
-  done
-  shift "$((OPTIND-1))"
-  case "$@" in
-    TLWR1043 | ARCHERC7 | TLWDR4300 | nand)
-      TARGET_PROFILE=$@
-      ;;
-    *)
-      show_help
-      exit 1
-  esac
-}
 
 clean_up() {
   rm -rf openwrt odin-driver-patches 
@@ -100,41 +59,7 @@ install_custom_feeds() {
 #   Kernel modules / Wireless drivers / kmod-ath / Atheros wireless debugging
 #   set the flag CONFIG_PACKAGE_ATH_DEBUG=y in the .conf file
 configure_openwrt() {
-  if test "$TARGET_PROFILE" == "nand" 
-  then			
-  make defconfig
-  sed -i.orig \
-      -e 's/\(CONFIG_TARGET_ar71xx_generic_Default\)=y/# \1 is not set/' \
-      -e "s/# \(CONFIG_TARGET_ar71xx_$TARGET_PROFILE\) is not set/\1=y/" \
-      -e 's/# \(CONFIG_PACKAGE_click\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_openvswitch\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_DEVEL\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_wireless-tools\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_luci\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_joe\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_nano\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_openvpn-nossl\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-openvswitch\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-tun\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-usb-storage\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-fs-ext4\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-fs-msdos\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-fs-vfat\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-nls-cp437\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-nls-iso8859-13\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-crypto-crc32c\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-lib-crc32c\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_tcpdump\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_hostapd\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_hostapd-utils\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_ATH_DEBUG\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-ath\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_kmod-ath9k-htc\) is not set/\1=y/' \
-      -e 's/\(CONFIG_PACKAGE_odhcp6c\)=y/# \1 is not set/' \
-      -e 's/\(CONFIG_PACKAGE_odhcpd\)=y/# \1 is not set/' \
-    .config
-  make defconfig
-  else
+  TARGET_PROFILE="TLWR1043"
   make defconfig
   sed -i.orig \
       -e 's/\(CONFIG_TARGET_ar71xx_generic_Default\)=y/# \1 is not set/' \
@@ -144,8 +69,6 @@ configure_openwrt() {
       -e 's/# \(CONFIG_DEVEL\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_wireless-tools\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_luci\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_joe\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_nano\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_openvpn-nossl\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_kmod-openvswitch\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_kmod-tun\) is not set/\1=y/' \
@@ -157,9 +80,6 @@ configure_openwrt() {
       -e 's/# \(CONFIG_PACKAGE_kmod-nls-iso8859-13\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_kmod-crypto-crc32c\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_kmod-lib-crc32c\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_tcpdump\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_hostapd\) is not set/\1=y/' \
-      -e 's/# \(CONFIG_PACKAGE_hostapd-utils\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_ATH_DEBUG\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_kmod-ath\) is not set/\1=y/' \
       -e 's/# \(CONFIG_PACKAGE_kmod-ath9k-htc\) is not set/\1=y/' \
@@ -167,8 +87,6 @@ configure_openwrt() {
       -e 's/\(CONFIG_PACKAGE_odhcpd\)=y/# \1 is not set/' \
     .config
   make defconfig	
-  fi
-  
  
 
 #Remove the "CONFIG_USE_MIPS16=y" option. Uncheck the MIPS16 option:
@@ -187,10 +105,9 @@ configure_openwrt() {
 }
 
 build() {
-  make $VERBOSE
+  make -j 4
 }
 
-parse_arguments "$@"
 clean_up
 clone_openwrt
 patch_ath9k
